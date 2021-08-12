4e601bda695a28ddd5aa58c15734b2264a276095
jakisondev <jakison.dev@gmail.com>
2021-08-12 09:31:47 -0300

    aula_04

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -43,7 +43,6 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         return () => {
             unsubscribe();
         }
-
     }, []);
 
     async function signInWithGoogle() {
